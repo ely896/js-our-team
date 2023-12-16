469b8fa86201ea58a88e4e7826aff09ec58427ab
ely896 <saretta2011@libero.it>
2023-12-16 11:22:06 +0100

    Add Milestone 2

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ const memberTeam = [
 //MILESTONE 1:
 //Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
 
-
+/*
 const memberTeam = [
     {
         nome: 'Wayne Barnett',
@@ -112,9 +112,67 @@ for (let i = 0; i < memberTeam.length; i++) {
     
 }
 
+*/
+//MILESTONE 2: Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+
+const memberTeam = [
+    {
+        nome: 'Wayne Barnett',
+        work: 'Founder & CEO',
+        image: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        nome: 'Angela Caroll',
+        work: 'Chief Editor',
+        image: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        nome: 'Walter Gordon',
+        work: 'Office Manager',
+        image: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        nome: 'Angela Lopez',
+        work: 'Office Manager',
+        image: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        nome: 'Scott Estrada',
+        work: 'Developer',
+        image: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        nome: 'Barbara Ramos',
+        work: 'Graphic Designer',
+        image: 'wayne-barnett-founder-ceo.jpg',
+    },
+]
+
+
+const rowDomElement = document.querySelector('.member .row')
+console.log(rowDomElement);
+
+
+for (let i = 0; i < memberTeam.length; i++) {
+    const member = memberTeam[i];
+
+    const markup = `
+    <div class="col-12 col-md-4">			
+        <div class="card">
+          <div class="card-body">
+                <h4>${member.image}</h4>
+                <h4>${member.nome}</h4>
+                <h4>${member.work}</h4>
+          </div>
+        </div>
+      </div>`
 
 
 
+    rowDomElement.insertAdjacentHTML('beforeend', markup)
 
+
+}
 
 
